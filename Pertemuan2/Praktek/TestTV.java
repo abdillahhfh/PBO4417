b65ac0bce63e520e01708d72789b91c7468f6607
abdillahhfh <72009291+abdillahhfh@users.noreply.github.com>
2021-03-10 12:47:04 +0700

    create TestTV.java

---
+++ Pertemuan2/Praktek/TestTV.java
@@ -0,0 +1,57 @@
+public class TestTV {
+    public static void main(String[] args) {
+        TV tv1 = new TV();
+        tv1.turnOn();
+        tv1.setChannel(30);
+        tv1.setVolume(3);
+
+        TV tv2 = new TV();
+        tv2.turnOn();
+        tv2.channelUp();
+        tv2.channelUp();
+        tv2.volumeUp();
+
+        System.out.println("tv1's channel is "+tv1.channel+" and volume level is "+ tv1.volumeLevel);
+        System.out.println("tv2's channel is "+tv2.channel+" and volume level is "+ tv2.volumeLevel);
+    }
+}
+
+class TV{
+    int channel;
+    int volumeLevel;
+    boolean on;
+
+    public TV(){}
+
+    public void turnOn() {
+        on = true;
+    }
+
+    public void turnOff() {
+        on = false;
+    }
+
+    public void setChannel(int newChannel) {
+       channel = newChannel;
+    }
+
+    public void setVolume(int newVolumeLevel) {
+        volumeLevel = newVolumeLevel;
+    }
+
+    public void channelUp() {
+        channel++;
+    }
+
+    public void channelDown() {
+        channel--;
+    }
+
+    public void volumeUp() {
+        volumeLevel++;
+    }
+
+    public void volumeDown() {
+        volumeLevel--;
+    }
+}
