b920efc1dbc3f5cbb6ee29ba2d840588a6517559
abdillahhfh <72009291+abdillahhfh@users.noreply.github.com>
2021-03-14 13:36:05 +0700

    Update TestTV.java

--- Pertemuan2/Praktek/TestTV.java
+++ Pertemuan2/Praktek/TestTV.java
@@ -1,18 +1,5 @@
 public class TestTV {
     public static void main(String[] args) {
-        // TV tv1 = new TV();
-        // tv1.turnOn();
-        // tv1.setChannel(30);
-        // tv1.setVolume(3);
-
-        // TV tv2 = new TV();
-        // tv2.turnOn();
-        // tv2.channelUp();
-        // tv2.channelUp();
-        // tv2.volumeUp();
-
-        // System.out.println("tv1's channel is "+tv1.channel+" and volume level is "+ tv1.volumeLevel);
-        // System.out.println("tv2's channel is "+tv2.channel+" and volume level is "+ tv2.volumeLevel);
 
         Manusia man1 = new Manusia("Abdillah");
         System.out.println(man1.namaSaya());
@@ -23,7 +10,6 @@ public class TestTV {
         System.out.println(man1.cekTV());
         man1.jualSemuaTV();
         System.out.println(man1.cekTV());
-
 
     }
 }
