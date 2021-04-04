e691da4406e0129aa6c0a89be0f78d65ee5d1497
abdillahhfh <72009291+abdillahhfh@users.noreply.github.com>
2021-04-04 19:32:52 +0700

    Create Teori
    Array, Stack, Queue

---
+++ Pertemuan5/src/Teori/Arr.java
@@ -0,0 +1,11 @@
+package Teori;
+import java.util.ArrayList;
+
+public class Arr {
+    ArrayList<String> isi = new ArrayList<String>(); 
+
+   public static void main(String[] args) {
+       System.out.println("Hello world");
+   }
+
+}
---
+++ Pertemuan5/src/Teori/Stck.java
@@ -0,0 +1,44 @@
+package Teori;
+
+import java.util.ArrayList;
+
+public class Stck {
+    ArrayList<String> isi = new ArrayList<String>();
+
+    Stck(){}
+
+    public void push(String data) {
+        isi.add(data);
+    }
+
+    public void pop(){
+        isi.remove(isi.size()-1);
+    }
+
+    public void size(){
+        System.out.println("Panjang list = " + isi.size());
+    }
+
+    public void peek() {
+        System.out.println("Data top = " + (isi.get(isi.size()-1)));
+    }
+
+    public void display(){
+        for (int i = 0; i < isi.size(); i++) {
+            System.out.print(isi.get(i) + " <-- ");
+        }
+    }
+    
+
+    public static void main(String[] args) {
+        Stck l1 = new Stck();
+        l1.push("Volvo");
+        l1.push("Scania");
+        l1.push("Daff");
+        l1.size();
+        l1.peek();
+        l1.display();
+        
+    }
+
+}
