ff102a3fc5ec8bb01487c76cf7e30b6e53146840
abdillahhfh <72009291+abdillahhfh@users.noreply.github.com>
2021-05-11 20:09:32 +0700

    Update

---
+++ Pertemuan8/src/com/SiaApp.java
@@ -0,0 +1,14 @@
+package com;
+
+public class SiaApp {
+    public static void main(String[] args) {       
+        Mahasiswa mhs1 = new Mahasiswa("Hajah","A0001");
+        mhs1.ambil_mk("Logika Informatika");
+        mhs1.ambil_mk("Basis Data");
+        mhs1.ambil_mk("Pancasila");
+        mhs1.get_info();
+
+    }
+
+
+}
