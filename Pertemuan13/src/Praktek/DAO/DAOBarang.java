b7d7be664c56599e24a2f92720ae006badbd4e1e
abdillahhfh <72009291+abdillahhfh@users.noreply.github.com>
2021-06-15 21:12:05 +0700

    Update DAOBarang.java

--- Pertemuan13/src/Praktek/DAO/DAOBarang.java
+++ Pertemuan13/src/Praktek/DAO/DAOBarang.java
@@ -5,10 +5,140 @@
  */
 package Praktek.DAO;
 
+import Praktek.Koneksi.Koneksi;
+import Praktek.Model.Barang;
+import Praktek.DAOInterface.IBarang;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author abdil
  */
-public class DAOBarang {
+public class DAOBarang implements IBarang{
+    Connection conn;
+    
+    final String insert = "INSERT INTO barang(id, nama, jumlah) VALUES (?,?,?);";
+    final String update = "UPDATE barang SET nama=?, jumlah=? WHERE id=?;";
+    final String delete = "DELETE FROM barang WHERE id=?;";
+    final String select = "SELECT * FROM barang;";
+    final String carinama = "SELECT * FROM barang WHERE nama LIKE ?;";
+    
+    public DAOBarang(){
+        conn = Koneksi.connection();
+    }
+
+    @Override
+    public void insert(Barang b) {
+        PreparedStatement statement = null;
+        try{
+            statement = conn.prepareStatement(insert);
+            statement.setInt(1, b.getId());
+            statement.setString(2, b.getNama());
+            statement.setInt(3, b.getJumlah());        
+            statement.executeUpdate();
+            ResultSet rs = statement.getGeneratedKeys();
+            while(rs.next()){
+                b.setId(rs.getInt(1));
+            }
+        } catch(SQLException ex){
+            System.out.println("Berhasil input");
+        } finally{
+            try{
+                statement.close();
+            } catch(SQLException ex){
+                System.out.println("Gagal Input");
+            }
+        }
+    }
+
+    @Override
+    public void update(Barang b) {
+        PreparedStatement statement = null;
+        try{
+            statement = conn.prepareStatement(update);
+            statement.setString(1, b.getNama());
+            statement.setInt(2, b.getJumlah());
+            statement.setInt(3, b.getId());
+            statement.executeUpdate();
+        } catch(SQLException ex){
+            System.out.println("Berhasil Update");
+        } finally{
+            try{
+                statement.close();
+            } catch(SQLException ex){
+                System.out.println("Gagal Update");
+            }
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        PreparedStatement statement = null;
+        try{
+            statement = conn.prepareStatement(delete);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch(SQLException ex){
+            System.out.println("Berhasil Delete");
+        } finally{
+            try{
+                statement.close();
+            }catch(SQLException ex){
+                System.out.println("Gagal Delete");
+            }
+        }
+    }
+
+    @Override
+    public List<Barang> getAll() {
+        List<Barang> list_barang = null;
+        try{
+            list_barang = new ArrayList<Barang>();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(select);
+            while(rs.next()){
+                Barang b = new Barang();
+                b.setId(rs.getInt("id"));
+                b.setNama(rs.getString("nama"));
+                b.setJumlah(rs.getInt("jumlah"));
+                list_barang.add(b);
+            }
+        } catch(SQLException ex){
+            Logger.getLogger(DAOBarang.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list_barang;
+    }
+
+    @Override
+    public List<Barang> getCariNama(String nama) {
+        List<Barang> list_barang = null;
+        try{
+            list_barang = new ArrayList<Barang>();
+            PreparedStatement st = conn.prepareStatement(carinama);
+            st.setString(1, "%" + nama + "%");
+            ResultSet rs = st.executeQuery();
+            while(rs.next()){
+                Barang b = new Barang();
+                b.setId(rs.getInt("id"));
+                b.setNama(rs.getString("nama"));
+                b.setJumlah(rs.getInt("jumlah"));
+                list_barang.add(b);
+            }
+            
+        }catch(SQLException ex){
+            Logger.getLogger(DAOBarang.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
     
 }
