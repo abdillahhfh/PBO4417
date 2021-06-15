b0f817be9e54ac4938c9c2b51ae97feef7b0e8dd
abdillahhfh <72009291+abdillahhfh@users.noreply.github.com>
2021-06-15 20:56:18 +0700

    Update TabelModelBarang.java

--- Pertemuan13/src/Praktek/Model/TabelModelBarang.java
+++ Pertemuan13/src/Praktek/Model/TabelModelBarang.java
@@ -4,11 +4,55 @@
  * and open the template in the editor.
  */
 package Praktek.Model;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
 
 /**
  *
  * @author abdil
  */
-public class TabelModelBarang {
+public class TabelModelBarang extends AbstractTableModel{
+    List<Barang> list_barang;
     
+    public TabelModelBarang(List<Barang> list_barang){
+        this.list_barang = list_barang;
+    }
+    
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+    
+    @Override
+    public int getRowCount() {
+        return list_barang.size();
+    }
+    
+    @Override
+    public String getColumnName(int column){
+        switch(column){
+            case 0:
+                return "ID";
+            case 1:
+                return "Nama";
+            case 2:
+                return "Jumlah";
+            default:
+                return null;
+        }
+    }
+    
+    @Override
+    public Object getValueAt(int row, int column) {
+        switch(column){
+            case 0:
+                return list_barang.get(row).getId();
+            case 1:
+                return list_barang.get(row).getNama();
+            case 2:
+                return list_barang.get(row).getJumlah();
+            default:
+                return null;
+        }
+    }
 }
